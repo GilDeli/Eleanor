076f0c621884a92ce41f714d1836152a6b881cfa
Aran Prylepski <aranhpril@gmail.com>
2026-01-30 13:23:03 +0300

    [up] v 0.0.0

--- main.py
+++ main.py
@@ -1,54 +1,32 @@
 import asyncio
-import subprocess
-import sys
 
-import modules.tts.tts_provider as tts_provider
-import modules.ai.ai_provider as ai_provider
-import modules.stt.stt_provider as stt_provider
-import modules.io as io 
+from src.modules.ai import AssistantCore
+from src.modules.output import OutAudio
+from src.modules.input import InputAudio
+from src.modules.stt import SttManager
+from src.modules.tts import TtsManager
+
+from src.modules.transform.txt import BufferSentences
+from src.modules.transform.audio import FixSynthetic
+
+from src.core.pipeline import PipelineLinear
+
 
 async def main():
-    agent_ai = ai_provider.Ollama()
-    agent_tts = tts_provider.Piper()
-    agent_stt = stt_provider.Vosk()
-    micro = io.Microphon()
-    mpv = io.Mpv()
-    txt_queue = asyncio.Queue()
-    audio_queue = asyncio.Queue()
-   
-    async def get_txt_stt():
-        audio_queue_micro = await micro.get_audio()
-        txt = await agent_stt.recognize_stream(audio_queue=audio_queue_micro)
-        return txt
-   
-    async def collect_ai_tokens():
-        txt = await get_txt_stt()
-        buffer = ""
-        punctuation = {'.', '!', '?', ';', ':'}
-        try:
-            async for token in agent_ai.generate_stream(txt):
-                buffer += token
-                if any(char in token for char in punctuation):
-                    await txt_queue.put(buffer)
-                    buffer = ""
-        finally:
-            if buffer != "":
-                await txt_queue.put(buffer)
-            await txt_queue.put(None)
+    ls = list() 
+    ls.append( InputAudio() )
+    ls.append( SttManager() )
+    ls.append( AssistantCore() )
+    ls.append( BufferSentences() )
+    ls.append( TtsManager() )
+    ls.append( FixSynthetic() )
+    ls.append( OutAudio() )
 
-    async def tts_synthesize():
-        try:
-            async for audio_chunk in agent_tts.synthesize_stream(txt_queue):
-                if isinstance(audio_chunk,Exception):
-                    print(f"Ошибка синтезации речи:{audio_chunk}")
-                    continue
-                if audio_chunk is not None:
-                    await audio_queue.put(audio_chunk)
-                while audio_queue.qsize() > 5: await asyncio.sleep(0.4)
-        finally:
-            await audio_queue.put(None)
+    pipeline = PipelineLinear(ls)
 
-    while True: await asyncio.gather(collect_ai_tokens(),tts_synthesize(),mpv.play_audio_stream(audio_queue))
+    while True:
+        await pipeline.start()
+        await pipeline.await_end()
     
 
 asyncio.run(main())
